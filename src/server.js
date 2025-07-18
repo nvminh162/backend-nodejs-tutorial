d61f71f9d7dbfc586988c7eb1a947905e38baa20
nvminh162 <nvminh162@gmail.com>
2025-07-18 22:03:13 +0700

    Add initial API and web route files
    - Created api.js in the routes directory for future API endpoints.
    - Created web.js in the routes directory to handle web requests and render the sample view.

--- src/server.js
+++ src/server.js
@@ -1,20 +1,17 @@
-// import exporess from "express"; // es modules
+require("dotenv").config();
 const express = require("express"); // commonjs
-const path = require("path");
-require('dotenv').config()
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
 const hostname = process.env.HOST_NAME; // hostname
 
 //config view engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+configViewEngine(app);
 
 // config routes
-app.get("/", (req, res) => {
-  res.render("sample");
-});
+app.use("/", webRoutes);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
