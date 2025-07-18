6973b7a61117f682fe572898c440c9fb055bd7ed
nvminh162 <nvminh162@gmail.com>
2025-07-18 18:20:32 +0700

    feat: add new route for nvminh162

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+
 const app = express()
 const port = 8081
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/nvminh162', (req, res) => {
+  res.send('@nvminh162!')
 })
 
 app.listen(port, () => {
