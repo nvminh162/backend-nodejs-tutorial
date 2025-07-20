d270a047adf9c8bea93994c3b2979e4d08585454
nvminh162 <nvminh162@gmail.com>
2025-07-20 23:38:15 +0700

    push dbs

--- install.md
+++ install.md
@@ -5,3 +5,5 @@ npm i --save-exact express@4.18.2
 npm install --save-exact ejs@3.1.8
 
 npm install --save-dev nodemon@2.0.20
+
+npm install --save-exact mysql2@2.3.3
--- src/server.js
+++ src/server.js
@@ -2,10 +2,23 @@ require("dotenv").config();
 const express = require("express"); // commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
 const hostname = process.env.HOST_NAME; // hostname
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PWD,
+});
+
+connection.query("select * from Users u", function (err, results, fields) {
+  console.log(results, fields);
+});
 
 //config view engine
 configViewEngine(app);
@@ -14,5 +27,5 @@ configViewEngine(app);
 app.use("/", webRoutes);
 
 app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
