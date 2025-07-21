19fc9c16c47df9716b7697e80cb1dae1fc579df8
nvminh162 <nvminh162@gmail.com>
2025-07-21 14:31:33 +0700

    feat: move database connection logic to a separate module

--- src/server.js
+++ src/server.js
@@ -2,22 +2,14 @@ require("dotenv").config();
 const express = require("express"); // commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-const mysql = require("mysql2");
+const connection = require("./config/database")
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
 const hostname = process.env.HOST_NAME; // hostname
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PWD,
-});
-
 connection.query("select * from Users u", function (err, results, fields) {
-  console.log(results, fields);
+  console.log(results);
 });
 
 //config view engine
