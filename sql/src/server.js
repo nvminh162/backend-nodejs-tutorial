926436b2e78799f0c1c4f5741cc588a7748e1412
nvminh162 <nvminh162@gmail.com>
2025-07-27 14:47:29 +0700

    feat: Initialize Express application with user management routes and views
    - Created API and web route files for user management.
    - Implemented server setup with Express and view engine configuration.
    - Developed user service for database operations (CRUD).
    - Added views for creating, updating, and displaying users.
    - Styled the home page and forms for better user experience.

---
+++ sql/src/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express"); // commonjs
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
+
+const app = express(); // app express
+const port = process.env.PORT || 8888; // port
+const hostname = process.env.HOST_NAME; // hostname
+
+// config req.body
+app.use(express.json( )); // Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
+//config view engine
+configViewEngine(app);
+
+// config routes
+app.use("/", webRoutes);
+
+app.listen(port, hostname, () => {
+  console.log(`App listening on port ${port}`);
+});
