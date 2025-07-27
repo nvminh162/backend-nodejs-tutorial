5d54bda752c71ec74134705a255ed7d727df91a3
nvminh162 <nvminh162@gmail.com>
2025-07-27 22:30:51 +0700

    feat: refactor database connection to use environment variables and improve error logging

--- nosql/src/server.js
+++ nosql/src/server.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express = require("express"); // commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-const connection = require('./config/database')
+const connection = require("./config/database");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
 const hostname = process.env.HOST_NAME; // hostname
 
 // config req.body
-app.use(express.json( )); // Used to parse JSON bodies
+app.use(express.json()); // Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 
 //config view engine
@@ -18,9 +18,10 @@ configViewEngine(app);
 // config routes
 app.use("/", webRoutes);
 
-// test connection
-connection();
-
-app.listen(port, hostname, () => {
-  console.log(`App listening on port ${port}`);
-});
+(async () => {
+  // test connection
+  await connection();
+  app.listen(port, hostname, () => {
+    console.log(`Backend @nvminh162 listening on port ${port}`);
+  });
+})();
