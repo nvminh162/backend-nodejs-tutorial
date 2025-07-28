4dae90cd14cab07ec28bc574d3d54147c1363ad8
nvminh162 <nvminh162@gmail.com>
2025-07-28 22:44:13 +0700

    feat: add customer model with schema definition for MongoDB integration

---
+++ nosql/src/models/customer.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    address: String,
+    phone: String,
+    email: String,
+    image: String,
+    description: String,
+  },
+  { timestamp: true }
+);
+
+const Customer = mongoose.model("customer", customerSchema);
+
+module.exports = Customer;
