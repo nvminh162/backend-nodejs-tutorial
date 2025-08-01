1ab341acd96f6ea45f8ac1191ed14371f8cba3d0
nvminh162 <nvminh162@gmail.com>
2025-08-01 22:00:43 +0700

    feat: update MongoDB dependency to version 4.13.0 and integrate MongoDB driver in server.js

--- nosql/src/server.js
+++ nosql/src/server.js
@@ -5,6 +5,7 @@ const webRoutes = require("./routes/web");
 const connection = require("./config/database");
 const apiRoutes = require("./routes/api");
 const fileUpload = require("express-fileupload");
+const { MongoClient } = require("mongodb");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
@@ -27,8 +28,17 @@ app.use("/", webRoutes);
 app.use("/v1/api", apiRoutes);
 
 (async () => {
-  // test connection
-  await connection();
+  // using mongoose
+  // await connection();
+
+  // using mongodb driver
+  const URL = process.env.DB_HOST_WITH_DRIVER;
+  const client = new MongoClient(URL);
+  await client.connect(); 
+  console.log('Connected successfully to server');
+  // const db = client.db(dbName);
+  // const collection = db.collection('documents');
+
   app.listen(port, hostname, () => {
     console.log(`Backend @nvminh162 listening on port ${port}`);
   });
